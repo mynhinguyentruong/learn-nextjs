4a41a9299d3181cca33ff045985b90e5372afcf8
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-10-01 10:20:32 -0400

    verify jwt token in client side

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import allPosts from "../data/posts.json";
-
+const jwt = require("jsonwebtoken");
 
 type Data = {
   allPosts: {
@@ -9,19 +9,19 @@ type Data = {
 };
 
 export default function Post({ allPosts }: Data) {
-
   const handleLogout = async () => {
     try {
-      await fetch('/api/logout', {
-        method: 'post',
-        headers: { "Content-Type": 'application/json'},
-        body: JSON.stringify({})
-      }).then((data) => console.log('data')).catch(err => console.log(err))
+      await fetch("/api/logout", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({}),
+      })
+        .then((data) => console.log("data"))
+        .catch((err) => console.log(err));
+    } catch (err) {
+      console.log(err);
     }
-    catch(err) {
-      console.log(err)
-    }
-  }
+  };
   return (
     <>
       <ul>
@@ -29,16 +29,39 @@ export default function Post({ allPosts }: Data) {
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
-      <button onClick={handleLogout} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4">
-  Sign Out
-</button>
+      <button
+        onClick={handleLogout}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+      >
+        Sign Out
+      </button>
     </>
   );
 }
 
 export async function getStaticProps() {
+  console.log("get static props ran");
+  // verify the token
+  try {
+    const decoded = await jwt.verify(
+      // verify the incoming token
+      `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZha2UuZW1haWxAZ21haWwuY29tIiwiaWF0IjoxNjY0NjMzODIwLCJleHAiOjE2NjQ2MzM4ODB9.lboyY7jpfvIlahpSqTxfZRDaLfZXxtJIM-7LF3r0ox8`,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+  } catch (err) {
+    console.log(err)
+    // handle error
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+        // statusCode: 301
+      },
+    };
+  }
+
   return {
     props: { allPosts }, // will be passed to the page component as prop
-    revalidate: 10
+    revalidate: 10,
   };
 }
